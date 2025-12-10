e346c1f3676afcb5de56531d6e12b4cb9701af02
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-12-10 13:47:15 +0200

    Fix application.java code

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,26 +1,41 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args)
-    {
+    public Application() {
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @PostConstruct
-    public void init()
-    {
-        Logger log = LoggerFactory.getLogger(Application.class);
+    public void init() {
         log.info("Java app started");
     }
 
+    @GetMapping({"/"})
+    public String home() {
+        return "It works!";
+    }
+
+    @GetMapping({"/status"})
     public String getStatus() {
         return "OK";
     }
