1caba5bf1c4ae5257c3571fa87dd99cf65074dfa
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-12-11 12:49:13 +0200

    update code

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,41 +1,26 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.example;
 
-import javax.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@RestController
 public class Application {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public Application() {
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Application.class, args);
     }
 
     @PostConstruct
-    public void init() {
+    public void init()
+    {
+        Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
     }
 
-    @GetMapping({"/"})
-    public String home() {
-        return "It works!!";
-    }
-
-    @GetMapping({"/status"})
     public String getStatus() {
         return "OK";
     }
