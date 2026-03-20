32aac7e920715f1c75980a550b72f67fe6858127
Nikolaos Zerzelidis <nickzerze@gmail.com>
2026-03-20 11:00:08 +0200

    update entry-script.sh to install docker compose in filder /usr/local/lib/docker

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -5,7 +5,8 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 # Install docker compose in Amazon Linux 2023 (https://docs.docker.com/compose/install/linux/#install-the-plugin-manually)
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+# DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+# mkdir -p $DOCKER_CONFIG/cli-plugins
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
