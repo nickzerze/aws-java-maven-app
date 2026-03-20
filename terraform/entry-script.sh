5f4ad830ae97905abb48ff9fc5aaf97b4c158ad5
Nikolaos Zerzelidis <nickzerze@gmail.com>
2026-03-20 10:38:09 +0200

    fix installation of docker compose in ec2-instance Amazon Linux 2023

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -4,5 +4,8 @@ sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
-# Install docker compose
-sudo yum install docker-compose-plugin
+# Install docker compose in Amazon Linux 2023 (https://docs.docker.com/compose/install/linux/#install-the-plugin-manually)
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
