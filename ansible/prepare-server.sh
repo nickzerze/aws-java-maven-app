cf8cbac38a0aa2bccd2dd1801f9d36d1763c834f
Nikolaos Zerzelidis <nickzerze@gmail.com>
2026-05-11 12:46:35 +0300

    Add Jenkinsfile for ansible execution 23

--- ansible/prepare-server.sh
+++ ansible/prepare-server.sh
@@ -3,7 +3,9 @@
 apt update
 apt install ansible -y
 apt install python3-pip -y
-pip3 install boto3 botocore
+#pip3 install boto3 botocore
+apt install python3-boto3 -y
+apt install python3-botocore -y
 
 # export AWS_ACCESS_KEY_ID=$1
 # export AWS_SECRET_ACCESS_KEY=$2
